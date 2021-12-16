1e9d7b5e018504e170b8e0d383515d56744b9e0e
Troy Barnes <troy@barnesyard.com>
2021-12-15 19:28:58 -0800

    Day 5 done

--- AOC2021/Day5.cs
+++ AOC2021/Day5.cs
@@ -2,25 +2,24 @@ class Day5 : AocDay
 {
     private string[] input;
     private List<Line> lines = new List<Line> { };
+    private int[,] grid1, grid2;
 
     private class Line
     {
-        private int x1;
-        private int x2;
-        private int y1;
-        private int y2;
+        private int[] linePoints = new int[4];
         public Line(int x1, int y1, int x2, int y2)
         {
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
+            this.linePoints[0] = x1;
+            this.linePoints[1] = y1;
+            this.linePoints[2] = x2;
+            this.linePoints[3] = y2;
         }
 
-        public int X1 { get { return this.x1; } }
-        public int Y1 { get { return this.y1; } }
-        public int X2 { get { return this.x2; } }
-        public int Y2 { get { return this.y2; } }
+        public int X1 { get { return this.linePoints[0]; } }
+        public int Y1 { get { return this.linePoints[1]; } }
+        public int X2 { get { return this.linePoints[2]; } }
+        public int Y2 { get { return this.linePoints[3]; } }
+        public int MaxPoint { get { return this.linePoints.Max(); } }
     }
 
     public Day5()
@@ -40,6 +39,17 @@ class Day5 : AocDay
             this.lines.Add(new Line(x1, y1, x2, y2));
         }
         Console.WriteLine("Stored " + this.lines.Count + " lines");
+
+        //search through all the lines and find the largest value for x or y point and use that to set size of grid array
+        int gridSize = 0;
+        foreach (Line oneLine in this.lines)
+        {
+            gridSize = oneLine.MaxPoint > gridSize ? oneLine.MaxPoint : gridSize;
+        }
+
+        grid1 = new int[gridSize + 1, gridSize + 1];
+        grid2 = new int[gridSize + 1, gridSize + 1];
+
     }
 
     public override void RunPartA()
@@ -47,19 +57,45 @@ class Day5 : AocDay
         Console.WriteLine("Running Day 5 part A");
 
         //loop over each line
-        //how many lines are horiz or vert?
-        int hOrVLines = 0;
         foreach (Line line in this.lines)
         {
-            //if the line is not vert or horiz skip it
-            if (line.X1 != line.X2 && line.Y1 != line.Y2)
+            int xit = 0;
+            int yit = 0;
+            if (line.X1 < line.X2) { xit = 1; }
+            else if (line.X1 > line.X2) { xit = -1; }
+            if (line.Y1 < line.Y2) { yit = 1; }
+            else if (line.Y1 > line.Y2) { yit = -1; }
+
+            int x = line.X1, y = line.Y1;
+            if (line.X1 == line.X2 || line.Y1 == line.Y2)
             {
-                continue;
+                this.grid1[x, y]++;
             }
-            Console.Write("Number of hor or ver lines: " + (hOrVLines++) + " in the list \r");
-            Thread.Sleep(5);
+            this.grid2[x, y]++;
+            do
+            {
+                x = x + xit;
+                y = y + yit;
+                this.grid2[x, y]++;
+                if (line.X1 == line.X2 || line.Y1 == line.Y2)
+                {
+                    this.grid1[x, y]++;
+                }
+            } while (x != line.X2 || y != line.Y2);
         }
-        Console.WriteLine();
+
+        int overlapCount1 = 0;
+        int overlapCount2 = 0;
+        for (int i = 0; i < this.grid1.GetLength(0); i++)
+        {
+            for (int j = 0; j < this.grid1.GetLength(1); j++)
+            {
+                if (this.grid1[i, j] > 1) { overlapCount1++; }
+                if (this.grid2[i, j] > 1) { overlapCount2++; }
+            }
+        }
+        Console.WriteLine("The number of overlapping grid points part 1: " + overlapCount1);
+        Console.WriteLine("The number of overlapping grid points part 2: " + overlapCount2);
     }
     public override void RunPartB()
     {
