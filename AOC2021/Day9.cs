30be88bce4196e4c55f2281c296e54523e9d7c2c
Troy Barnes <troy@barnesyard.com>
2021-12-24 16:15:44 -0800

    Day 9 complete

--- AOC2021/Day9.cs
+++ AOC2021/Day9.cs
@@ -1,47 +1,73 @@
 class Day9 : AocDay
 {
     private string[] input;
-    private int[,] htGrid;
+    private GridPoint[,] htGrid;
     private int gridWidth;
     private int gridHeight;
     private List<int> basinSizes = new List<int> { };
+
+    private class GridPoint
+    {
+        public int heightValue { get; set; }
+        public int basinNumber { get; set; }
+
+        public GridPoint()
+        {
+            heightValue = 0;
+            basinNumber = 0;
+        }
+    }
 
     public Day9()
     {
         //Load the input
         this.input = System.IO.File.ReadAllLines(@".\input\Day9_Input.txt");
-        htGrid = new int[this.input.Length, this.input[0].Length];
+        // Since I am going to use 1 as my first basin I need to add the 0 basin size to start:
+        this.basinSizes.Add(0);
         gridHeight = this.input.Length;
         gridWidth = this.input[0].Length;
+        htGrid = new GridPoint[gridHeight, gridWidth];
         for (int r = 0; r < gridHeight; r++)
         {
             for (int c = 0; c < gridWidth; c++)
             {
-                htGrid[r, c] = int.Parse(this.input[r][c].ToString());
+                htGrid[r, c] = new GridPoint();
+                htGrid[r, c].heightValue = int.Parse(this.input[r][c].ToString());
             }
         }
     }
 
     private bool isLowPt(int r, int c)
     {
-        bool checkLeft = c == 0 ? true : (htGrid[r, c] < htGrid[r, c - 1]);
-        bool checkRight = c == gridWidth - 1 ? true : (htGrid[r, c] < htGrid[r, c + 1]);
-        bool checkUp = r == 0 ? true : (htGrid[r, c] < htGrid[r - 1, c]);
-        bool checkDown = r == gridHeight - 1 ? true : (htGrid[r, c] < htGrid[r + 1, c]);
-
-        if (checkLeft && checkRight && checkUp && checkDown)
-        {
-            return true;
-        }
-        return false;
-
+        int value = htGrid[r, c].heightValue;
+        if (c > 0 && (value >= htGrid[r, c - 1].heightValue)) { return false; }
+        if (c < gridWidth - 1 && (value >= htGrid[r, c + 1].heightValue)) { return false; }
+        if (r > 0 && (value >= htGrid[r - 1, c].heightValue)) { return false; }
+        if (r < gridHeight - 1 && (value >= htGrid[r + 1, c].heightValue)) { return false; }
+        return true;
     }
 
-    private int getBasinSize(int r, int c)
+    private void setBasinNumber(int r, int c, int basinNum)
     {
-        // if the left point is less than 9 it adds 1 to our basin size
-        int checkLeft = (c != 0) && (htGrid[r, c - 1] < 9) ? getBasinSize(r, c - 1) + 1 : 0;
-        return 1;
+        if (r < 0 || c < 0) { return; }
+        if (r >= this.gridHeight || c >= this.gridWidth) { return; }
+        if (this.htGrid[r, c].basinNumber != 0)
+        {
+            // This point was already tagged in a basin
+            return;
+        }
+
+        if (this.htGrid[r, c].heightValue < 9)
+        {
+            this.htGrid[r, c].basinNumber = basinNum;
+            this.basinSizes[basinNum]++;
+
+            setBasinNumber(r + 1, c, basinNum);
+            setBasinNumber(r - 1, c, basinNum);
+            setBasinNumber(r, c - 1, basinNum);
+            setBasinNumber(r, c + 1, basinNum);
+        }
+
     }
 
     public override void SolveDay()
@@ -50,6 +76,9 @@ class Day9 : AocDay
 
         // Track the sum of the risk level of each low point as you find them
         int lowPointsSum = 0;
+
+        // Use a counter for numbering each basin
+        int currentBasin = 1;
 
         //loop over the entire grid of elevations
         for (int r = 0; r < gridHeight; r++)
@@ -61,21 +90,31 @@ class Day9 : AocDay
                 bool isLow = false;
                 if (isLowPt(r, c))
                 {
-                    lowPointsSum += htGrid[r, c] + 1;
+                    lowPointsSum += htGrid[r, c].heightValue + 1;
                     isLow = true;
 
-                    // find the size of the basin
-                    int basinSize = 1 + this.getBasinSize(r, c);
-                    this.basinSizes.Add(basinSize);
+                    // if a low point is found add another basin to our list of basin sizes
+                    this.basinSizes.Add(0);
+                    // find the size of the basin for this point and the connected points
+                    this.setBasinNumber(r, c, currentBasin);
+
+                    //increment the basin number for the next low point
+                    currentBasin++;
                 }
                 if (isLow) { Console.ForegroundColor = ConsoleColor.Red; }
                 else { Console.ForegroundColor = ConsoleColor.White; }
-                Console.Write(htGrid[r, c]);
+                Console.Write(htGrid[r, c].heightValue);
             }
             Console.Write("\n");
         }
         Console.ResetColor();
         Console.WriteLine("Sum of low point risk: " + lowPointsSum);
+        int[] sortedBasinSizes = new int[basinSizes.Count];
+        this.basinSizes.Sort();
+        int size1 = this.basinSizes[this.basinSizes.Count - 1];
+        int size2 = this.basinSizes[this.basinSizes.Count - 2];
+        int size3 = this.basinSizes[this.basinSizes.Count - 3];
+        Console.WriteLine("Top 3 basin sizes: " + size1 + " " + size2 + " " + size3 + " And multiply them to get: " + (size1 * size2 * size3));
     }
 
 }
