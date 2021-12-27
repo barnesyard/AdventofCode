ba6aec096bb9c3d405b40054a179ea4e75007add
Troy Barnes <troy@barnesyard.com>
2021-12-27 10:59:56 -0800

    Finixhed day 11 both parts

--- AOC2021/Day11.cs
+++ AOC2021/Day11.cs
@@ -2,6 +2,7 @@ class Day11 : AocDay
 {
     private string[] input;
     private int[,] octogrid = new int[10, 10];
+    private int[] stats = new int[10];
 
     public Day11()
     {
@@ -21,16 +22,32 @@ class Day11 : AocDay
     {
         Console.WriteLine("Getting the solution to Day11");
         int flashes = 0;
+        bool foundSyncFlash = false;
+        int loop = 0;
         // do this 100 times
-        for (int i = 0; i < 100; i++)
+        do
         {
+            // Each loop check to see if we have a synced flash
+            int[] stateCounts = new int[10];
+
             // First increase all the energy levels by 1
             for (int r = 0; r < 10; r++)
             {
                 for (int c = 0; c < 10; c++)
                 {
+                    // Before taking this step collect stats on previous step's results
+                    // while all values are in range 0-9 to avoid "out of range" error
+                    // increment the count and evaluate if it is a sync value
+                    // the sync value will not be > 99 until the energy value is incremented so looking for 98
+                    if (stateCounts[octogrid[r, c]]++ > 98)
+                    {
+                        foundSyncFlash = true;
+                        Console.WriteLine("On step " + loop + " the flash synced across all dumbos!");
+                        Environment.Exit(0);
+                    }
+
+                    // Now increment the energy value
                     octogrid[r, c]++;
-                    //renderGrid();
                 }
             }
 
@@ -59,12 +76,18 @@ class Day11 : AocDay
                         flashes += spreadFlashEnergy(r + 1, c);
                         flashes += spreadFlashEnergy(r + 1, c + 1);
                     }
-                    //renderGrid();
                 }
             }
-            // Let's see if that actually did what it was supposed to do
-            Console.WriteLine("The count of flashes: " + flashes);
+
+            // Part 1 wants count of flashes on day 100
+            if (loop < 100)
+            {
+                Console.WriteLine("At step " + loop + " the count of flashes: " + flashes);
+            }
+
+            loop++;
         }
+        while (!foundSyncFlash);
     }
 
     private int spreadFlashEnergy(int r, int c)
@@ -110,7 +133,12 @@ class Day11 : AocDay
             }
             Console.Write("\n");
         }
-        Thread.Sleep(1);
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Energy level: " + i + " and count for value: " + this.stats[i]);
+        }
+
+        Thread.Sleep(2);
     }
 
 
