c3769edb920aba4791c7fe93cde42dce6431d693
Troy Barnes <troy@barnesyard.com>
2021-12-26 16:59:22 -0800

    Day 10 part1 failure

--- AOC2021/Day10.cs
+++ AOC2021/Day10.cs
@@ -12,6 +12,52 @@ class Day10 : AocDay
     {
         Console.WriteLine("Getting the solution to Day10");
 
+        int syntaxErrorScore = 0;
+
+        foreach (string line in this.input)
+        {
+            Dictionary<char, int> chunkTracking = new Dictionary<char, int> { };
+            chunkTracking.Add('(', 0);
+            chunkTracking.Add(')', 0);
+            chunkTracking.Add('[', 0);
+            chunkTracking.Add(']', 0);
+            chunkTracking.Add('<', 0);
+            chunkTracking.Add('>', 0);
+            chunkTracking.Add('{', 0);
+            chunkTracking.Add('}', 0);
+
+            // ): 3 points
+            // ]: 57 points
+            // }: 1197 points
+            // >: 25137 points
+            foreach (char c in line)
+            {
+                chunkTracking[c]++;
+                if (chunkTracking[')'] > chunkTracking[')'])
+                {
+                    syntaxErrorScore += 3;
+                    break;
+                }
+                if (chunkTracking[']'] > chunkTracking['['])
+                {
+                    syntaxErrorScore += 57;
+                    break;
+                }
+                if (chunkTracking['}'] > chunkTracking['{'])
+                {
+                    syntaxErrorScore += 1197;
+                    break;
+                }
+                if (chunkTracking['>'] > chunkTracking['<'])
+                {
+                    syntaxErrorScore += 25137;
+                    break;
+                }
+            }
+            Console.WriteLine("Current syntax score: " + syntaxErrorScore);
+        }
+        // Output the final value for the syntax error score
+        Console.WriteLine("Final Score: " + syntaxErrorScore);
     }
 
 }
